3d343c47dd4b189f64cac4c7f1a70579ce9a0080
syynch <sbulk9@gmail.com>
2015-02-10 17:03:46 +0000

    Added & Linked Screens

---
+++ WAF/Classes/ShopScene.h
@@ -0,0 +1,14 @@
+#ifndef __SHOPSCENE_H__
+#define __SHOPSCENE_H__
+
+#include "cocos2d.h"
+
+class ShopScene: public cocos2d::CCScene
+{
+public:
+	CREATE_FUNC(ShopScene);
+	virtual bool init();
+};
+
+
+#endif // __SHOPSCENE_H__
